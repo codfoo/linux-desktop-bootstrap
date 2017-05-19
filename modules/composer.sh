092883fa26737c1f4e498fd96551816435c5e609
Rahul Kalkani <kalkanirb@gmail.com>
2017-05-19 21:35:55 +0530

    Add ufw and composer

---
+++ modules/composer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# name = ""
+# supported_versions = ""
+# description = ""
+
+_run_default() {
+
+    if [[ ! -z "$DEBUG" ]]; then
+		echo 'curl -sS https://getcomposer.org/installer | php'
+	else
+		curl -sS https://getcomposer.org/installer | php
+	fi
+
+	$DEBUG mv composer.phar /usr/local/bin/composer
+	$DEBUG chmod a+x /usr/local/bin/composer
+}
+
+_run_() {
+    _run_default
+}
+
+# TODO
+if $(which _run_$1); then
+    echo "ERROR: $0 Version $1 is not available."
+    exit;
+fi
+
+_run_$1
