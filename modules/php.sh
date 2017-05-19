affb5c4c7caf7a498ba4399e7afc92834196a66c
Rahul Kalkani <kalkanirb@gmail.com>
2017-05-19 21:33:00 +0530

    Add php module

---
+++ modules/php.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# name = ""
+# supported_versions = ""
+# description = ""
+
+_run_default() {
+    # Commands
+    $DEBUG sudo apt-get install -y --force-yes php-cli php-dev php-sqlite3 php-gd \
+    			php-curl php-memcached php-imap php-mysql php-mbstring php-soap \
+    			php-bcmath php-xml php-zip php-intl php-readline php-mcrypt
+}
+
+_run_() {
+    _run_default
+}
+
+# TODO
+if $(which _run_$1); then
+    echo "ERROR: $0 Version $1 is not available."
+    exit;
+fi
+
+_run_$1
