1af5f5db03048d5775a3f9566b886973ad4e7f51
Rahul Kalkani <kalkanirb@gmail.com>
2017-05-19 21:34:15 +0530

    Add basic utilities and supervisor

---
+++ modules/supervisor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# name = ""
+# supported_versions = ""
+# description = ""
+
+_run_default() {
+    # Commands
+    $DEBUG sudo apt-get install -y --force-yes supervisor
+    $DEBUG systemctl enable supervisor.service
+    $DEBUG service supervisor start
+}
+
+_run_() {
+    _run_default
+}
+
+# TODO
+if $(which _run_$1); then
+    echo "ERROR: $0 Version $1 is not available."
+    exit;
+fi
+
+_run_$1
---
+++ modules/utilities.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# name = ""
+# supported_versions = ""
+# description = ""
+
+_run_default() {
+    $DEBUG sudo apt-get install -y --force-yes libmcrypt4 unzip whois curl
+}
+
+_run_() {
+    _run_default
+}
+
+# TODO
+if $(which _run_$1); then
+    echo "ERROR: $0 Version $1 is not available."
+    exit;
+fi
+
+_run_$1
