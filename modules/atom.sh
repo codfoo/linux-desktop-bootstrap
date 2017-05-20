bb041fb896db546ec41607e2676f25e9f4f73fdc
Rahul Kalkani <kalkanirb@gmail.com>
2017-05-20 12:35:07 +0530

    I9 Add atom module

---
+++ modules/atom.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# name = ""
+# supported_versions = ""
+# description = ""
+
+_run_default() {
+    $DEBUG cd ~/Downloads
+    $DEBUG wget https://github.com/atom/atom/releases/download/v1.17.0/atom-amd64.deb
+    $DEBUG sudo dpkg -i atom-amd64.deb
+}
+
+_run_() {
+    _run_default
+}
+
+# TODO
+if $(which _run_$1); then
+    echo "ERROR: $0 Version $1 is not available."
+    exit;
+fi
+
+_run_$1
